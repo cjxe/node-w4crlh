58e975fac797d26839292b09272cce7f26ffdf4f
cjxe <MahmutBaranTurkmen@gmail.com>
2023-11-17 13:50:50 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 className="text-center">UI Components playgrounddd</h1>
+      </header>
+
+      <div>hello</div>
+    </div>
+  );
+}
+
+export default App;
